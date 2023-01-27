56e067fa22a8ce3aa495cddad53a1201aa3d3ba4
Bernardo Escudeiro <73354104+jbtescudeiro16@users.noreply.github.com>
2023-01-27 16:09:00 +0000

    Delete testeskika directory

--- testeskika/src/Controlador.java
+++
@@ -1,8 +0,0 @@
-public interface Controlador {
-    void reserva(int testeId, int[] salaIds) throws InterruptedException; //docente reserva conjunto de salas
-    boolean presenca(int testeId);// aluno regista presena no teste
-    void entrega(int testeId);// aluno entrega o seu teste
-    int comear_limpeza();// obter sala para limpeza
-    void terminar_limpeza(int salaId);//fim de limpeza de sala
-}
-
--- testeskika/src/Reuniao.java
+++
@@ -1,7 +0,0 @@
-public interface Reuniao {
-    void participa(int lista) throws InterruptedException;
-    void abandona(int lista);
-    int naSala();
-    int aEspera();
-
-}
